8147e3578139e1637a6a477e1d8d3029b757e878
bluesky <852155110@qq.com>
2021-07-23 00:26:00 +0800

    📝 增加 【字符串】-->【字符串转换整数 (atoi)】

--- LeetCode/LeetCode/String/StringAtoi.swift
+++ LeetCode/LeetCode/String/StringAtoi.swift
@@ -14,7 +14,7 @@
  检查下一个字符（假设还未到字符末尾）为正还是负号，读取该字符（如果有）。 确定最终结果是负数还是正数。 如果两者都不存在，则假定结果为正。
  读入下一个字符，直到到达下一个非数字字符或到达输入的结尾。字符串的其余部分将被忽略。
  将前面步骤读入的这些数字转换为整数（即，"123" -> 123， "0032" -> 32）。如果没有读入数字，则整数为 0 。必要时更改符号（从步骤 2 开始）。
- 如果整数数超过 32 位有符号整数范围 [−231,  231 − 1] ，需要截断这个整数，使其保持在这个范围内。具体来说，小于 −231 的整数应该被固定为 −231 ，大于 231 − 1 的整数应该被固定为 231 − 1 。
+ 如果整数数超过 32 位有符号整数范围 [−2^31,  2^31 − 1] ，需要截断这个整数，使其保持在这个范围内。具体来说，小于 −2^31 的整数应该被固定为 −2^31 ，大于 2^31 − 1 的整数应该被固定为 2^31 − 1 。
  返回整数作为最终结果。
  */
 
@@ -24,9 +24,8 @@ class StringAtoi {
         let str = s.trimmingCharacters(in: .whitespaces)
         guard str.count > 0 else { return 0 }
         let strArray = Array(str)
-        let firstChar = strArray.first!;
         // 判断首字符合法
-        guard (firstChar.isNumber || firstChar == "+" || firstChar == "-") else {
+        if let firstChar = strArray.first, !firstChar.isNumber && firstChar != "+" && firstChar != "-" {
             return 0
         }
         var resultStr = ""
@@ -44,12 +43,7 @@ class StringAtoi {
             }
         }
         // 判断结果字符串合法
-        guard resultStr != "+" else {
-            return 0
-        }
-        guard resultStr != "-" else {
-            return 0
-        }
+        guard resultStr != "+", resultStr != "-" else { return 0 }
         // 判断是否溢出
         let resultInt = Int32(resultStr)
         guard resultInt != nil else {
